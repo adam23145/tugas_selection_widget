7ebce38a57249252b0c889f93d66ac0901da2c44
chaplin100 <81203366+chaplin100@users.noreply.github.com>
2021-10-06 20:07:28 +0700

    Adam Dwi Julianto

--- app/src/main/java/com/example/tugas/DaftarMenu.java
+++ app/src/main/java/com/example/tugas/DaftarMenu.java
@@ -7,19 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class DaftarMenu extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_menu);
         ListView listView = (ListView) findViewById(R.id.listView);
 
-
+        button = findViewById(R.id.buttonback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         String[] menu = {"Movie"};
         ArrayAdapter<String> myAdapter =  new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menu);
